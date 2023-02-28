a3acd5e38d3d6c6d9cba03dedb5362144c708452
SUBBULAKSHMIL <727721euit162@skcet.ac.in>
2023-02-28 15:46:11 +0530

    Day7

---
+++ Day7/src/main/java/com/example/demo/service/ApiService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Car;
+import com.example.demo.repository.CarRepo;
+
+@Service
+public class ApiService {
+	
+	@Autowired
+	CarRepo repo;
+	public boolean postCar(Car car) {
+		repo.save(car);
+		return true;
+	}
+
+	public List<Car> getCarsByOwners(int owners) {
+		return repo.findByOwners(owners);
+	}
+
+	public List<Car> getCarByAddress(String address) {
+		return repo.findByAddress(address);
+	}
+
+	public List<Car> getCarByCarName(String carname) {
+		return repo.findByCarName(carname);
+	}
+
+	public List<Car> getCarByOwnersAndCarType(int owners, String cartype) {
+		return repo.findByOwnersAndCarType(owners, cartype);
+	}
+
+}
